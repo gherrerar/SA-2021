995de5bc5e972f4d68881bfb5cc9b01e1198570b
gherrerar <gabi.gh597@gmail.com>
2021-07-01 20:13:11 -0300

    Finalização do Swiper + Responsividade

--- src/main/resources/static/js/swiperInit.js
+++ src/main/resources/static/js/swiperInit.js
@@ -1,14 +1,31 @@
-const swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
+var swiper = new Swiper('.swiper-container', {
+    direction: getDirection(),
+    effect: 'fade',
+    slidesPerView: 1,
+    spaceBetween: 60,
     loop: true,
-    centeredSlides: true,
+    autoplay: {
+        disableOnInteraction: true,
+    },
+    // centeredSlides: true,
     keyboard: true,
+    mousewheel: false,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     pagination: {
-        el: '.swiper-slider__pagination',
-        clickable: true
+        el: '.swiper-pagination',
+        clickable: true,
     },
+    on: {
+        resize: function(){
+            swiper.changeDirection(getDirection())
+        }
+    }
 })
+
+function getDirection() {
+    var direction = window.innerWidth <= 996 ? 'horizontal' : 'vertical'
+    return direction;
+  }
